e96569bb22a6b39bc25440ecc17cb3d0583e875a
banutsav <utsavb7@gmail.com>
2019-07-29 15:58:24 +0530

    exploring plotly express

--- charts/views.py
+++ charts/views.py
@@ -7,7 +7,7 @@ from charts.models import EblityTopicTable
 def index_page(request):
 	# Get Topic data for Grade 8
 	topics = pd.DataFrame(list(EblityTopicTable.objects.filter(grade=8).order_by('sequence')
-		.values_list('topic_name','sequence','month','hours',named=True)))
+		.values_list('topic_name','sequence','month','hours', 'weightage',named=True)))
 	# Create figure from data
 	fig = viz.createScatter(topics) 
 	return render(request,'charts/index.html',{'fig':fig})
--- charts/viz.py
+++ charts/viz.py
@@ -2,6 +2,7 @@ import plotly.express as px
 import plotly.offline as opy
 
 def createScatter(topics):
-	fig = px.scatter(topics, x='sequence', y='hours', color='month')
+	#print(topics['month'].unique())
+	fig = px.scatter(topics, x='sequence', y='month', size='hours', color='weightage')
 	plot = opy.plot(fig, auto_open=False, output_type='div', include_plotlyjs=False)
 	return plot
